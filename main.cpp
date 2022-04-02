a1be8b3d91924b64e0e626b24e9b3ab6218bbc87
marcusphilips <marcellfulop2000@gmail.com>
2022-04-02 10:18:53 -0700

    Code added to navigate files

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
+/*
+Made by Marcell Fulop for Citrus Hack 2022
+*/
 #include <Magick++.h>
 #include <iostream>
+#include <string>
+#include <filesystem>
 
 using namespace std;
 using namespace Magick;
 
+
+struct Pixel{
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+};
+
 int main(int argc, char** argv){
+    // need to capture original image files
+    string filepath = "./";
+    if (argc < 2){
+        cout << "Filepath argument missing" << endl;
+        return 1;
+    }
+    char* f = argv[1];
+    unsigned i = 0;
+    while (f[i] != '\0'){
+        filepath.push_back(f[i]);
+        i++;
+    }
+    filepath.push_back('/');
+    cout << filepath << endl;
+    // point to the folder that contains all the files
+    InitializeMagick(*argv);
+
     return 0;
 }
